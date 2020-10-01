d3a37686645bc43c1ab5b7d2bc8ad7dacde81ae2
Clara Gyökeres <you@example.com>
2020-10-01 22:31:54 +0200

    bla

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import "sammie.png"
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      🌹 GRATTIS ÄLSKLING! 🌹
+      Jag kan inte vara där och ge dig kramar idag även om jag skulle vilja mest av allt. 💌  Istället vill jag bjuda dig på en avkopplande massage i London.💆‍♂️ Du väljer och det är min treat! 💰 Älskar dig baby det är du så värd. 🍸  Tusen kramar till dig på födelsedagen! 💚
     </div>
   )
 }
